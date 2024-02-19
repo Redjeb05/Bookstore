ef7be9252762a82f6e636788a5f810c49d8001cf
Redjeb Ali <aliredjeb728@gmail.com>
2024-02-19 09:02:43 +0200

    Add project files.

---
+++ Bookstore/Data/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookstore.Data
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+
+        public IList<Book> BooksList { get; set; }
+         public IList<Genre> GenreList { get; set; }
+
+    }
+}
